23ce9b7b737283b126a5044f39b5c5fcfa06b0d9
hinalehriya-jpg <hinalehriya@gmail.com>
2026-02-19 01:43:40 +0530

    ecosytem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'makanview',
+      script: 'npm',
+      args: 'start',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      max_memory_restart: '500M',
+      error_file: './logs/pm2-error.log',
+      out_file: './logs/pm2-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+    }
+  ]
+};
